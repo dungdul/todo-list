7c2f31000675caee05836c29b31c4ea852eef418
Dung Dul <178075100+dungdul@users.noreply.github.com>
2026-04-05 14:43:05 +0900

    Move all classes into a separate file

--- src/index.js
+++ src/index.js
@@ -1,126 +1,20 @@
 import './styles.css';
+import {Todo} from './classes.js';
 
-class TodoItem {
-  constructor(title, description, dueDate, priority, projectId) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.description = description;
-    this.dueDate = new Date(dueDate);
-    this.priority = priority;
-    this.projectId = projectId;
-    this.completed = false;
-  }
+window.todo = new Todo();
+window.Todo = Todo;
 
-  toggleCompleted() {
-    this.completed = !this.completed;
-  }
-}
+window.p1 = window.todo.addProject('p1');
+let t1 = p1.addTodoItem('thing 1', 'some blah', '2020-01-03', 'low');
+let t2 = p1.addTodoItem('thing 2', 'tnersitnareio', '2020-01-03', 'medium');
+let t3 = p1.addTodoItem('thing 3', 'arsnetio', '2025-02-01', 'low');
 
-class Project {
-  constructor(title) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.todoItems = [];
-    this.archived = false;
-  }
+window.p2 = window.todo.addProject('p2');
+window.p3 = window.todo.addProject('p3');
+window.p3.toggleArchived();
 
-  addTodoItem(title, description, dueDate, priority) {
-    const todoItem = new TodoItem(title, description, dueDate, priority, this.id);
-    this.todoItems.push(todoItem);
-
-    // Return the todo item so that it can ge stored in a variable and easily accessed when using console
-    return todoItem;
-  }
-
-  deleteTodoItem(todoItemId) {
-    this.todoItems = this.todoItems.filter(currentItem => currentItem.id !== todoItemId);
-  }
-
-  getTodoItem(todoItemId) {
-    return this.todoItems.find(currentItem => currentItem.id === todoItemId);
-  }
-
-  toggleArchived() {
-    this.archived = !this.archived;
-  }
-}
-
-class Todo {
-  constructor() {
-    this.projects = [];
-  }
-
-  addProject(projectTitle) {
-    const project = new Project(projectTitle);
-    this.projects.push(project);
-
-    // Return the project object so that it can be stored in a variable and can be accessed via console
-    return project;
-  }
-
-  deleteProject(projectId) {
-    this.projects = this.projects.filter(currentProject => currentProject.id !== projectId);
-  }
-
-  getProject(projectId) {
-    return this.projects.find(currentProject => currentProject.id === projectId);
-  }
-
-  getUnarchivedProjects() {
-    return this.projects.filter(project => project.archived === false);
-  }
-
-  getArchivedProjects() {
-    return this.projects.filter(project => project.archived === true);
-  }
-
-  printTodoList() {
-    this.projects.forEach((project, index) => {
-      console.log(`Project ${index + 1}: ${project.title}`);
-      console.log(`  ID: ${project.id}`);
-      console.log(`  Archived: ${project.archived}`);
-      project.todoItems.forEach((todoItem, index) => {
-        console.log(`  Item ${index + 1}: ${todoItem.title}`);
-        console.log(`    ID: ${todoItem.id}`);
-        console.log(`    ${todoItem.description}`);
-        console.log(`    ${todoItem.dueDate}`);
-        console.log(`    ${todoItem.priority}`);
-        console.log(`    Completed: ${todoItem.completed}`);
-      })
-    })
-  }
-
-  static groupTodoItems(todoItems, propertyToGroupBy) {
-    const groups = [];
-
-    function createGroup(title) {
-      return {
-        title,
-        todoItems: [],
-      }
-    }
-
-    todoItems.forEach(item => {
-      // Find a group that has the same propertyToGroupBy's value as the current item
-      // If found, it will add the current item to the group. If not found, it will create a new group before adding the item
-      // For example, if the propertyToGroupBy is dueDate, it will find the group with title of current item's due date
-
-      // Turn the value into a string first. Otherwise, the comparison won't work
-      const value = String(item[propertyToGroupBy]);
-
-      const groupFound = groups.find(group => group.title === value);
-      if (groupFound) {
-        groupFound.todoItems.push(item);
-      } else {
-        const newGroup = createGroup(value);
-        newGroup.todoItems.push(item);
-        groups.push(newGroup);
-      }
-    });
-
-    return groups;
-  }
-}
+let t21 = p2.addTodoItem('this is active', 'ntrseaistoreia', '2020-09-09', 'high');
+let t31 = p3.addTodoItem('this is archived', 'luy;f3pwfpluy', '2018-02-02', 'low');
 
 function renderSidebar() {
   const projectContainerDiv = document.querySelector('.project-container');
@@ -235,22 +129,6 @@ function renderPage(title, todoItems) {
   });
 }
 
-window.todo = new Todo();
-window.Todo = Todo;
-
-window.p1 = window.todo.addProject('p1');
-let t1 = p1.addTodoItem('thing 1', 'some blah', '2020-01-03', 'low');
-let t2 = p1.addTodoItem('thing 2', 'tnersitnareio', '2020-01-03', 'medium');
-let t3 = p1.addTodoItem('thing 3', 'arsnetio', '2025-02-01', 'low');
-
-window.p2 = window.todo.addProject('p2');
-window.p3 = window.todo.addProject('p3');
-window.p3.toggleArchived();
-
-let t21 = p2.addTodoItem('this is active', 'ntrseaistoreia', '2020-09-09', 'high');
-let t31 = p3.addTodoItem('this is archived', 'luy;f3pwfpluy', '2018-02-02', 'low');
-
 renderSidebar();
 addEventListenersToSidebar();
-let g1 = Todo.groupTodoItems(p1.todoItems, document.querySelector('#group-by').value);
 renderPage(p1.title, p1.todoItems);
